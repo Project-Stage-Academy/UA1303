b9564ec7507e76e8680276d8e0e007c9be3619b7
Bohdan Forkutsa <bogdan200085@gmail.com>
2024-12-15 16:50:52 +0200

    Defined Django models for Project, Media and Description

--- forum/profiles/models.py
+++ forum/profiles/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 from users.models import User
 
@@ -20,4 +21,18 @@ class StartupProfile(models.Model):
 
 
 class InvestorProfile(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="investor_profile")
+    country = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=20)
+    address = models.CharField(max_length=250)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+    account_balance = models.DecimalField(
+        max_digits=15, 
+        decimal_places=2, 
+        default=0.00, 
+        validators=[MinValueValidator(0.00)]
+        )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- forum/projects/models.py
+++ forum/projects/models.py
@@ -1,3 +1,30 @@
+from django.core.validators import MinValueValidator
 from django.db import models
+from profiles.models import StartupProfile
 
 # Create your models here.
+
+class Project(models.Model):
+    startup = models.ForeignKey(StartupProfile, on_delete=models.CASCADE, related_name="project")
+    title = models.CharField(max_length=100)
+    funding_goal = models.DecimalField(
+        max_digits=15, 
+        decimal_places=2, 
+        validators=[MinValueValidator(0.00)]
+        )
+    is_published = models.BooleanField(default=False)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Media(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="media")
+    file = models.ImageField()
+
+
+class Description(models.Model):
+    project = models.OneToOneField(Project, on_delete=models.CASCADE, related_name="description")
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
