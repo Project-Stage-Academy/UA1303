4b026904b8d9ea4c7a3923f25f6e6e49aa6ceac6
Bohdan Forkutsa <bogdan200085@gmail.com>
2024-12-17 15:55:43 +0200

    corrections according to amendments

--- forum/profiles/models.py
+++ forum/profiles/models.py
@@ -1,21 +1,22 @@
 from django.core.validators import MinValueValidator
 from django.db import models
 from users.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 
 class StartupProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="startup_profile")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="startup_profile", db_index=True)
     company_name = models.CharField(max_length=200)
     industry = models.CharField(max_length=100)
     size = models.CharField(max_length=50)
     country = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
     zip_code = models.CharField(max_length=20)
-    address = models.CharField(max_length=250)
-    phone = models.CharField(max_length=20)
-    email = models.EmailField(unique=True)
-    description = models.TextField(blank=True, null=True)
+    address = models.CharField(max_length=250, blank=True, null=True)
+    phone = PhoneNumberField(null=True, blank=True)
+    email = models.EmailField(unique=True, db_index=True)
+    description = models.TextField(max_length=1000, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -36,13 +37,13 @@ class StartupProfile(models.Model):
         )
 
 class InvestorProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="investor_profile")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="investor_profile", db_index=True)
     country = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
     zip_code = models.CharField(max_length=20)
-    address = models.CharField(max_length=250)
-    phone = models.CharField(max_length=20)
-    email = models.EmailField(unique=True)
+    address = models.CharField(max_length=250, blank=True, null=True)
+    phone = PhoneNumberField(null=True, blank=True)
+    email = models.EmailField(unique=True, db_index=True)
     account_balance = models.DecimalField(
         max_digits=15, 
         decimal_places=2, 
@@ -57,6 +58,7 @@ class InvestorProfile(models.Model):
         verbose_name = "Investor Profile"
         verbose_name_plural = "Investor Profiles"
         ordering = ["-created_at"]
+        constraints = [models.CheckConstraint(check=models.Q(account_balance__gte=0), name='account_balance_non_negative')]
 
     def __str__(self):
         return (
--- forum/projects/models.py
+++ forum/projects/models.py
@@ -1,19 +1,39 @@
+from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator
 from django.db import models
+from PIL import Image
 from profiles.models import StartupProfile
 
+def validate_image(file):
+    try:
+        img = Image.open(file)
+        img.verify()
+    except Exception:
+        raise ValidationError("Uploaded file is not a valid image.")
+    
+    valid_extensions = ["jpg", "jpeg", "png"]
+    if not file.name.split(".")[-1].lower() in valid_extensions:
+        raise ValidationError(f"Only {", ".join(valid_extensions)} files are allowed.")
+    
+    if file.size > 5 * 1024 * 1024:
+        raise ValidationError("The file size must not exceed 5MB.")
 # Create your models here.
 
 class Project(models.Model):
-    startup = models.ForeignKey(StartupProfile, on_delete=models.CASCADE, related_name="project")
+    startup = models.ForeignKey(
+        StartupProfile, 
+        on_delete=models.CASCADE, 
+        related_name="projects", 
+        db_index=True
+        )
     title = models.CharField(max_length=100)
     funding_goal = models.DecimalField(
         max_digits=15, 
         decimal_places=2, 
         validators=[MinValueValidator(0.00)]
         )
-    is_published = models.BooleanField(default=False)
-    is_completed = models.BooleanField(default=False)
+    is_published = models.BooleanField(default=False, db_index=True)
+    is_completed = models.BooleanField(default=False, db_index=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -21,6 +41,7 @@ class Project(models.Model):
         verbose_name = "Project"
         verbose_name_plural = "Projects"
         ordering = ['-created_at']
+        constraints = [models.CheckConstraint(check=models.Q(funding_goal__gte=0), name="funding_goal_non_negative")]
 
     def __str__(self):
         return (
@@ -34,7 +55,12 @@ class Project(models.Model):
 
 class Media(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="media")
-    file = models.ImageField()
+    file = models.ImageField(validators=[validate_image])
+
+    def __str__(self):
+        return f"Media(id={self.pk}, startup={self.project.startup.company_name}, project={self.project.title}, file={self.file})"
+    
+        
 
 
 class Description(models.Model):
@@ -42,3 +68,6 @@ class Description(models.Model):
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Description(id={self.pk}, startup={self.project.startup.company_name}, project={self.project.title}, description={self.description})"
