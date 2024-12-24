993eb576a169e97f3c1624aa10f45260c628c877
Ira <iramladanovych@gmail.com>
2024-12-24 11:54:08 +0200

    Implement notification models, serializers, views, urls & tests

--- forum/notifications/admin.py
+++ forum/notifications/admin.py
@@ -1,15 +1,3 @@
 from django.contrib import admin
 
-from .models import NotificationType, NotificationPreference
-
-
-@admin.register(NotificationType)
-class NotificationTypeAdmin(admin.ModelAdmin):
-    list_display = ['name', 'description']
-
-
-@admin.register(NotificationPreference)
-class NotificationPreferenceAdmin(admin.ModelAdmin):
-    list_display = ['user']
-    filter_horizontal = ['allowed_notification_types']
-
+# Register your models here.
