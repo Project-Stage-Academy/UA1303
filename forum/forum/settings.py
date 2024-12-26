6a391f6c1665e53b7f32c77aff7e8370b00ef38f
devdsys <dimakyrytschuk@gmail.com>
2024-12-27 01:39:33 +0200

    Prevent log duplication by disabling propagation to root logger

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -228,6 +228,7 @@ LOGGING = {
         'django': {
             'handlers': ['console', 'forum_file'],
             'level': 'WARNING',
+            'propagate': False,
         },
         'django.db.backends': {
             'handlers': ['console', 'database_file'],
