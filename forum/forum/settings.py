6adfd1f4d0c4878a3ef6157f04855adb8fcc94e3
Ira <iramladanovych@gmail.com>
2025-01-04 08:36:35 +0200

    Implement password recovery functionality

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
-
 import environ
 from dotenv import load_dotenv
 
@@ -58,6 +57,7 @@ INSTALLED_APPS = [
     "communications",
     "dashboard",
     "notifications",
+    "channels",
 ]
 
 MIDDLEWARE = [
@@ -91,9 +91,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "forum.wsgi.application"
 
-AUTH_USER_MODEL = 'users.CustomUser'
+AUTH_USER_MODEL = "users.CustomUser"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
 
 # DATABASES = {
 #     'default': {
@@ -101,6 +102,7 @@ AUTH_USER_MODEL = 'users.CustomUser'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
 
 DATABASES = {
     "default": {
@@ -121,7 +123,8 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', 'OPTIONS': {'min_length': 8}
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {"min_length": 8},
     },
     {
         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
@@ -149,8 +152,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = []
 
 # Default primary key field type
@@ -159,27 +162,27 @@ STATICFILES_DIRS = []
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
 }
 
 DJOSER = {
-    'PASSWORD_RESET_CONFIRM_URL': 'password-reset/{uid}/{token}/',
-    'SEND_ACTIVATION_EMAIL': False,
-    'PASSWORD_RESET_SHOW_EMAIL_NOT_FOUND': True,
+    "PASSWORD_RESET_CONFIRM_URL": "password-reset/{uid}/{token}/",
+    "SEND_ACTIVATION_EMAIL": False,
+    "PASSWORD_RESET_SHOW_EMAIL_NOT_FOUND": True,
 }
 
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_HSTS_SECONDS = 3600
-SECURE_SSL_REDIRECT = os.getenv('DJANGO_SECURE_SSL_REDIRECT', 'True') == 'True'
 
-LOG_DIR = os.path.join(BASE_DIR, 'logs')
+SECURE_SSL_REDIRECT = os.getenv("DJANGO_SECURE_SSL_REDIRECT", "True") == "True"
+
+LOG_DIR = os.path.join(BASE_DIR, "logs")
 
 try:
     os.makedirs(LOG_DIR, exist_ok=True)
@@ -187,104 +190,85 @@ except OSError as e:
     raise RuntimeError(f"Failed to create log directory {LOG_DIR}: {e}")
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '{asctime} {levelname} {name} {message}',
-            'style': '{',
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{asctime} {levelname} {name} {message}",
+            "style": "{",
         },
-        'simple': {
-            'format': '{levelname} {name} {message}',
-            'style': '{',
-        },
-    },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple',
-        },
-        'forum_file': {
-            'level': 'INFO',
-            'class': 'logging.handlers.TimedRotatingFileHandler',
-            'filename': os.path.join(LOG_DIR, 'forum.log'),
-            'when': 'midnight',
-            'backupCount': 7,
-            'formatter': 'verbose',
-            'delay': True,
-        },
-        'database_file': {
-            'level': 'INFO',
-            'class': 'logging.handlers.TimedRotatingFileHandler',
-            'filename': os.path.join(LOG_DIR, 'database.log'),
-            'when': 'midnight',
-            'backupCount': 7,
-            'formatter': 'verbose',
-            'delay': True,
+        "simple": {
+            "format": "{levelname} {name} {message}",
+            "style": "{",
         },
     },
-    'loggers': {
-        'django': {
-            'handlers': ['console', 'forum_file'],
-            'level': 'WARNING',
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "simple",
         },
-        'django.db.backends': {
-            'handlers': ['console', 'database_file'],
-            'level': 'INFO',
-            'propagate': False,
+        "forum_file": {
+            "level": "INFO",
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            "filename": os.path.join(LOG_DIR, "forum.log"),
+            "when": "midnight",
+            "backupCount": 7,
+            "formatter": "verbose",
+            "delay": True,
         },
-        '': {
-            'handlers': ['console', 'forum_file'],
-            'level': 'DEBUG',
-            'propagate': True,
+        "database_file": {
+            "level": "INFO",
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            "filename": os.path.join(LOG_DIR, "database.log"),
+            "when": "midnight",
+            "backupCount": 7,
+            "formatter": "verbose",
+            "delay": True,
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console", "forum_file"],
+            "level": "WARNING",
+        },
+        "django.db.backends": {
+            "handlers": ["console", "database_file"],
+            "level": "INFO",
+            "propagate": False,
+        },
+        "": {
+            "handlers": ["console", "forum_file"],
+            "level": "DEBUG",
+            "propagate": True,
         },
     },
 }
 
 # JWT settings
+
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=20),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': True,
-    'BLACKLIST_AFTER_ROTATION': True,
-    'UPDATE_LAST_LOGIN': False,
-    'USER_ID_FIELD': 'user_id',
-    'ALGORITHM': 'HS256',
-    'SIGNING_KEY': os.getenv('JWT_SIGNING_KEY', SECRET_KEY),
-    'AUDIENCE': None,
-    'ISSUER': None,
-    'JSON_ENCODER': None,
-    'JWK_URL': None,
-    'LEEWAY': 0,
-    'AUTH_HEADER_TYPES': ('JWT', 'Bearer'),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=20),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "UPDATE_LAST_LOGIN": False,
+    "USER_ID_FIELD": "user_id",
+    "ALGORITHM": "HS256",
+    "SIGNING_KEY": os.getenv("JWT_SIGNING_KEY", SECRET_KEY),
+    "AUDIENCE": None,
+    "ISSUER": None,
+    "JSON_ENCODER": None,
+    "JWK_URL": None,
+    "LEEWAY": 0,
+    "AUTH_HEADER_TYPES": ("JWT", "Bearer"),
 }
 
-# Swagger settings to enable JWT authorization
-SWAGGER_SETTINGS = {
-    'SECURITY_DEFINITIONS': {
-        'Bearer': {
-            'type': 'apiKey',
-            'name': 'Authorization',
-            'in': 'header',
-            'description': 'Enter: "Bearer <JWT token>"',
-        }
-    },
-    'USE_SESSION_AUTH': False,
-}
 
-# Daphne settings
-ASGI_APPLICATION = "forum.asgi.application"
-
-
-# User settings
 AUTH_USER_MODEL = "users.CustomUser"
-
-# reCAPTCHA settings
 RECAPTCHA_PUBLIC_KEY = env("RECAPTCHA_PUBLIC_KEY")
 RECAPTCHA_PRIVATE_KEY = env("RECAPTCHA_PRIVATE_KEY")
 
-# Email settings
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = env("EMAIL_HOST")
 EMAIL_PORT = env.int("EMAIL_PORT")
@@ -293,10 +277,40 @@ EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
 
-# Rate limiting settings
 RATE_LIMIT_KEY = "ip"
 RATE_LIMIT_RATE = "5/m"
 RATE_LIMIT_BLOCK = True
-
-# Domain settings
 DOMAIN_NAME = os.getenv("DOMAIN_NAME", "localhost")
+
+# Swagger settings to enable JWT authorization
+SWAGGER_SETTINGS = {
+    "SECURITY_DEFINITIONS": {
+        "Bearer": {
+            "type": "apiKey",
+            "name": "Authorization",
+            "in": "header",
+            "description": 'Enter: "Bearer <JWT token>"',
+        }
+    },
+    "USE_SESSION_AUTH": False,
+}
+
+
+# Daphne
+ASGI_APPLICATION = "forum.asgi.application"
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "unique-snowflake",
+    }
+}
+
+RATELIMIT_USE_CACHE = "default"  # Make sure RATELIMIT is reconfigured to use Redis when we add this type of caching
