cb1560c569c4af5c65a08a0dd4caad5df3a265fc
devdsys <dimakyrytschuk@gmail.com>
2025-01-22 15:28:08 +0200

    up

--- forum/profiles/tests/test_views.py
+++ forum/profiles/tests/test_views.py
@@ -10,20 +10,14 @@ class NotificationsAPITests(APITestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.investor_1 = InvestorProfileFactory.create()
-        cls.investor_2 = InvestorProfileFactory.create()
-        cls.startup = StartupProfileFactory.create()
+        cls.investor_1 = InvestorProfileFactory()
+        cls.investor_2 = InvestorProfileFactory()
+        cls.startup = StartupProfileFactory()
 
         cls.auth_header_startup = generate_auth_header(cls.startup.user, 1)
         cls.auth_header_investor_1 = generate_auth_header(cls.investor_1.user, 2)
         cls.auth_header_investor_2 = generate_auth_header(cls.investor_2.user, 2)
 
     def test_basic(self):
-        url = reverse('profiles:startups-save-favorite', kwargs={'pk': self.startup.pk})
-        response = self.client.post(url, **self.auth_header_investor_1)
-        self.assertIn('is saved', response.json()['detail'])
-
-        url = reverse('profiles:startup-profile-detail', kwargs={'pk': self.startup.pk})
-        response = self.client.get(url, **self.auth_header_startup)
-
-        print(response.json())
+        url = reverse('profiles:startups-view-startup', kwargs={'pk': self.startup.pk})
+        response = self.client.get(url, **self.auth_header_investor_1)
